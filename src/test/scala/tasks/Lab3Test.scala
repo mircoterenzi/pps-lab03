8c954a5a65b4eef2c69b18493d207f5f1a70ea7b
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-24 15:47:21 +0100

    refactor tests

--- src/test/scala/tasks/Lab3Test.scala
+++ src/test/scala/tasks/Lab3Test.scala
@@ -6,11 +6,8 @@ import tasks.Lab3.*
 import u02.Modules.Person.*
 import u03.Sequences.Sequence.*
 import u03.Streams.*
-import u03.Streams.Stream.*
 
-class Lab3Test:
-
-  // -------------------------------------------------- TASK 2 --------------------------------------------------
+class Task2Test:
 
   val sequence = Cons(Teacher("Viroli", "PPS"), Cons(Teacher("Ricci", "PCD"),
       Cons(Student("Terenzi", 2025), Cons(Teacher("Viroli", "OOP"), Nil()))))
@@ -25,12 +22,11 @@ class Lab3Test:
   @Test def testGetNumberCourses() =
     assertEquals(3, getNumberCourses(sequence))
 
-  // -------------------------------------------------- TASK 3 --------------------------------------------------
+class Task3Test:
 
   @Test def testFill() =
     assertEquals(Cons("a", Cons("a", Cons("a", Nil()))), Stream.toList(Stream.fill(3)("a")))
 
   @Test def testFibonacci() =
-    val fib5 = Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Nil())))))
     val fibonacci: Stream[Int] = Stream.fibonacci()
     assertEquals(Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Nil()))))), Stream.toList(Stream.take(fibonacci)(5)))
